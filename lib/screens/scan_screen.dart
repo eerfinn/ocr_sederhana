c02cd8b01fd8ad72fc93be407a8039298ec4722c
eerfinn <erfinbrian@gmail.com>
2025-10-21 13:55:59 +0700

    UTS: Selesai Soal 2 - Tampilan Loading dan Error

--- lib/screens/scan_screen.dart
+++ lib/screens/scan_screen.dart
@@ -76,7 +76,7 @@ class _ScanScreenState extends State<ScanScreen> {
     } catch (e) {
       if (!mounted) return;
       ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Error saat mengambil/memproses foto: $e')),
+        const SnackBar(content: Text('Pemindaian Gagal! Periksa Izin Kamera atau coba lagi.')),
       );
     }
   }
@@ -84,7 +84,22 @@ class _ScanScreenState extends State<ScanScreen> {
   @override
   Widget build(BuildContext context) {
     if (!_isCameraInitialized) {
-      return const Scaffold(body: Center(child: CircularProgressIndicator()));
+      return Scaffold(
+        backgroundColor: Colors.grey[900],
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              CircularProgressIndicator(color: Colors.yellow),
+              SizedBox(height: 16),
+              Text(
+                'Memuat Kamera... Harap tunggu.',
+                style: TextStyle(color: Colors.white, fontSize: 18),
+              ),
+            ],
+          ),
+        ),
+      );
     }
 
     return Scaffold(
