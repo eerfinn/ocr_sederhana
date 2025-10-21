51df273fcc2914bdfe0cb308e833fee01f952901
eerfinn <erfinbrian@gmail.com>
2025-10-21 14:24:13 +0700

    UTS: Selesai Soal 3 - Implementasi Flutter TTS

--- lib/screens/result_screen.dart
+++ lib/screens/result_screen.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 import 'home_screen.dart';
 
-class ResultScreen extends StatelessWidget {
+class ResultScreen extends StatefulWidget {
   final String ocrText;
 
   const ResultScreen({super.key, required this.ocrText});
+
+  @override
+  State<ResultScreen> createState() => _ResultScreenState();
+}
+
+class _ResultScreenState extends State<ResultScreen> {
+  late FlutterTts flutterTts;
+
+  @override
+  void initState() {
+    super.initState();
+    flutterTts = FlutterTts();    
+    flutterTts.setLanguage('id-ID');
+  }
+
+  @override
+  void dispose() {
+    flutterTts.stop();
+    super.dispose();
+  }
+
+  Future<void> _speak() async {
+    if (widget.ocrText.isNotEmpty) {
+      await flutterTts.speak(widget.ocrText);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,22 +41,34 @@ class ResultScreen extends StatelessWidget {
         padding: const EdgeInsets.all(16.0),
         child: SingleChildScrollView(
           child: SelectableText(
-            ocrText.isEmpty
+            widget.ocrText.isEmpty
                 ? 'Tidak ada teks ditemukan.'
-                : ocrText,
+                : widget.ocrText,
             style: const TextStyle(fontSize: 18),
           ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.pushAndRemoveUntil(
-            context,
-            MaterialPageRoute(builder: (_) => const HomeScreen()),
-            (route) => false,
-          );
-        },
-        child: const Icon(Icons.home),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: _speak,
+            heroTag: 'tts',
+            child: const Icon(Icons.volume_up),
+          ),
+          const SizedBox(height: 16),
+          FloatingActionButton(
+            onPressed: () {
+              Navigator.pushAndRemoveUntil(
+                context,
+                MaterialPageRoute(builder: (_) => const HomeScreen()),
+                (route) => false,
+              );
+            },
+            heroTag: 'home',
+            child: const Icon(Icons.home),
+          ),
+        ],
       ),
     );
   }
